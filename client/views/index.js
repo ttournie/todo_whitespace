eee5f62babd2647d80f6a39defbc4b6593797ace
thierry Tournie <thierry.tournie92@gmail.com>
2015-12-19 16:06:23 +0100

    changing tasks display template

--- client/views/index.js
+++ client/views/index.js
@@ -1,14 +1,33 @@
+// Index event.
 Template.index.events({
     'submit form': function(e){
 		e.preventDefault();
 
+    // Get the fom content.
 		var content = $("textarea[name='contenu']").val();
+    var text = event.target.text.value;
 
-		var task = {
-				content: content,
-        owner: Meteor.userId(),           // _id of logged in user
-        username: Meteor.user().username  // username of logged in user
-		}
-        Tasks.insert(task);
+    // Insert a task.
+    Meteor.call("addTask", text);
+
+    // Clear form the form.
+    event.target.text.value = "";
 	}
+});
+
+// Meteor methods called by server.
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
 });
