91f06e302c714564498bde939a24b3567301d680
thierry Tournie <thierry.tournie92@gmail.com>
2015-12-19 16:32:56 +0100

    resolving error on insert

--- client/views/index.js
+++ client/views/index.js
@@ -4,30 +4,12 @@ Template.index.events({
 		e.preventDefault();
 
     // Get the fom content.
-		var content = $("textarea[name='contenu']").val();
-    var text = event.target.text.value;
+    var text = e.target.content.value;
 
     // Insert a task.
-    Meteor.call("addTask", text);
+    Meteor.call('addTask', text);
 
     // Clear form the form.
-    event.target.text.value = "";
+    e.target.content.value = "";
 	}
-});
-
-// Meteor methods called by server.
-Meteor.methods({
-  addTask: function (text) {
-    // Make sure the user is logged in before inserting a task
-    if (! Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-
-    Tasks.insert({
-      text: text,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
-  },
 });
--- server/publications.js
+++ server/publications.js
@@ -3,3 +3,21 @@ Meteor.publish("allTasksHeaders", function(){
         fields: {content: 0}
     });
 });
+
+
+// Meteor methods.
+Meteor.methods({
+  addTask: function(text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      content: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+});
